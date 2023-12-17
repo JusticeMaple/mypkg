6151b6ecc8d565a2be4d6f5fd641d7c18eac1bcd
Tei <justice_zwy@yahoo.co.jp>
2023-12-17 14:36:15 +0900

    change talker.listener

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -4,15 +4,33 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
-import sys
-print(sys.version)
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %s" % msg.data)
+from person_msgs.srv import Query
 
-rclpy.init()
-node = Node("listener")
-pub = node.create_subscription(Int16, "countup", cb, 10)
+def main():
+    rclpy.init()
+    node = Node("listener")
+    client = node.create_client(Query, 'query') 
+    while not client.wait_for_service(timeout_sec=1.0):
+        node.get_logger().info('待機中')
 
-rclpy.spin(node)
+    req = Query.Request()
+    req.name = "鄭韻遠"
+    future = client.call_async(req)
+
+    while rclpy.ok():
+        rclpy.spin_once(node)
+        if future.done():
+            try:
+                response = future.result()
+            except:
+                node.get_logger().info('呼び出し失敗')
+            else:
+                node.get_logger().info("age: {}".format(response.age))
+
+            break
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
