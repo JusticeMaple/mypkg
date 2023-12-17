80e09f8f9ee1990223bf8ad00a66f7a7b65c93bd
Tei <justice_zwy@yahoo.co.jp>
2023-12-17 16:23:14 +0900

    Change a little

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -5,14 +5,18 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
-import sys
-print(sys.version)
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %s" % msg.data)
 
 rclpy.init()
-node = Node("listener")
-pub = node.create_subscription(Int16, "countup", cb, 10)
+node = Node("talker")
+pub = node.create_publisher(Int16, "countup", 10)
+n = 0
 
+def cb():
+    global n
+    msg = Int16()
+    msg.data = n
+    pub.publish(msg)
+    n += 1
+
+node.create_timer(0.5, cb)
 rclpy.spin(node)
