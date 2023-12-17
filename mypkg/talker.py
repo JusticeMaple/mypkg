86e9bf3fe195fd037f7f7232cdd59ee7cae5fa2b
Tei <justice_zwy@yahoo.co.jp>
2023-12-17 20:17:30 +0900

    fix

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -4,13 +4,13 @@
 
 import rclpy
 from rclpy.node import Node
+
 from std_msgs.msg import Int16
 
 rclpy.init()
 node = Node("talker")
 pub = node.create_publisher(Int16, "countup", 10)
 n = 0
-
 def cb():
     global n
     msg = Int16()
