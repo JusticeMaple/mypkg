45cfbbe28c26837bb5ec8e7b1bf25f05951d03e7
Tei <justice_zwy@yahoo.co.jp>
2023-12-17 20:10:23 +0900

    change main

---
+++ mypkg/build/mypkg/build/lib/mypkg/listener.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: © 2023 Yunyuan_Zheng
+# SPDX-License-Identifier: BSD-3-Clause
+
+import rclpy
+from rclpy.node import Node
+from person_msgs.srv import Query
+
+def main():
+    rclpy.init()
+    node = Node("listener")
+    client = node.create_client(Query, 'query')
+    while not client.wait_for_service(timeout_sec=1.0):
+        node.get_logger().info('待機中')
+
+    req = Query.Request()
+    req.name = "鄭韻遠"
+    future = client.call_async(req)
+
+    while rclpy.ok():
+        rclpy.spin_once(node)
+        if future.done():
+            try:
+                response = future.result()
+            except:
+                node.get_logger().info('呼び出し失敗')
+            else:
+                node.get_logger().info("age: {}".format(response.age))
+
+            break
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ mypkg/install/mypkg/lib/python3.10/site-packages/mypkg/talker.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: © 2023 Yunyuan_Zheng
+# SPDX-License-Identifier: BSD-3-Clause
+
+import rclpy
+from rclpy.node import Node
+from person_msgs.srv import Query
+
+def cb(request, response):
+    if request.name == "鄭韻遠":
+        response.age = 20
+    else:
+        response.age = 255
+
+    return response
+
+rclpy.init()
+node = Node("talker")
+srv = node.create_service(Query, "query", cb)
+rclpy.spin(node)
